08fa3af739da978c20ca27a83371004adeebd348
Alex Louzao <alexlouzao@desktop-umpqin0.highpoint.edu>
2017-03-01 11:07:58 -0500

    FIXED adding new Q/A

--- quiz/ViewController.swift
+++ quiz/ViewController.swift
@@ -8,59 +8,45 @@
 
 import UIKit
 
+var q: String = ""
+var a: String = ""
+var questions: [String] = ["From what is cognac is made?", "What is 7+7?",
+                           "What is the capital of Vermont?"]
+var answers: [String] = ["Grapes", "14", "Montpelier"]
 
 class ViewController: UIViewController {
     
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
-    
-    var q: String! = ""
-    var a: String! = ""
-    
+
     @IBOutlet var questionField: UITextField!
     @IBOutlet var answerField: UITextField!
-    
-    var questions: [String] = ["From what is cognac is made?", "What is 7+7?",
-                               "What is the capital of Vermont?"]
-    
-    var answers: [String] = ["Grapes", "14", "Montpelier"]
     
     var currentQuestionIndex: Int = 0
     
     
     @IBAction func showNextQuestion(_ sender: UIButton) {
-        print("Question NewQ = \(q)")
+        //print("Question NewQ = \(q)")
         currentQuestionIndex += 1
         if currentQuestionIndex == questions.count {
            currentQuestionIndex = 0
-        }
-        for i in questions{
-            print("In Next Question: \(i)")
         }
         questionLabel.text = questions[currentQuestionIndex]
         answerLabel.text = "???"
     }
     
     @IBAction func showAnswer(_ sender: UIButton) {
-        print("Answer NewA = \(a)")
+        //print("Answer NewA = \(a)")
         let answer: String = answers[currentQuestionIndex]
         answerLabel.text = answer
     }
     
     override func viewDidLoad() {
-        //super.viewDidLoad()
-        print("Original Questions:")
+        super.viewDidLoad()
+        print("Questions:")
         for i in questions{
             print("\(i)")
         }
-        q = "STUPID"
-        a = "MONKEY"
-        //newQ = retQ()
-        //newA = retA
-        questions.append(q)
-        answers.append(a)
-        print("View Load NewQ = \(q)")
-        //questionLabel.text = questions[currentQuestionIndex]
     }
     
     
@@ -69,13 +55,12 @@ class ViewController: UIViewController {
     @IBAction func setNewQandA(_ sender: UIButton){
         q = questionField.text!
         a = answerField.text!
-        if q != nil && a != nil{
+        if q != "" && a != ""{
             // add question and answer to the arrays
             print("Adding new elements: \(q) and \(a)")
             questions.append(q) // addNewQ
             answers.append(a)   // addNewA
-            //showNextQuestion(sender)
-            // have function to clear the text fields
+            clearFields()
         } else {
             print("BAD INPUT")
         }
@@ -85,74 +70,13 @@ class ViewController: UIViewController {
         for i in answers{
             print("Answer: \(i)")
         }
-        //viewDidLoad()
-        
+        // call function to clear text fields
     }
     
-
-    
-    
-    
-    
-    
-    
-    
-    //var otherView = newQuestionAnswer()
-    
-    /*@IBAction func getQuestion(_ textField: UITextField){
-        newQ = textField.text
-        if newQ == ""{
-            newQ = nil
-        }
-        print("In new Question ViewCon: \(newQ)")
+    func clearFields(){
+        questionField.text = ""
+        answerField.text = ""
     }
-    
-    @IBAction func getAnswer(_ textField: UITextField){
-        newA = textField.text
-        if newA == ""{
-            newA = nil
-        }
-        print("In new Answer ViewCon: \(newA)")
-    }
-    func addQ(){
-        questions.append(newQ)
-    }
-    func addA(){
-        answers.append(newA)
-    }
-    
-    // Set the tag of each text field to different values
-    // Find a way to clear the text field after press and after addition
-    
-    
-    // let newQuestion set the new question and answer
-    // have simple function here to append the new values
-    
-    @IBAction func setQandANew(_ sender: UIButton){
-        //newQ = otherView.newQ
-        //newA = otherView.newA
-        print("Button Press Q: \(newQ)")
-        print("Button Press A: \(newA)")
-        if (newQ != nil && newA != nil) {
-            // add question and answer to the arrays
-            print("Adding new elements: \(newQ) and \(newA)")
-            addQ()
-            addA()
-            // have function to clear the text fields
-        }
-        for i in questions{
-            print("Question: \(i)")
-        }
-        for i in answers{
-            print("Answer: \(i)")
-        }
-        viewDidLoad()
-    }*/
-    
-    
-    
-    
-    
 
 }
     
